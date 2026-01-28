b0f3866d2821d5bd15bc4bff0105503c0723db9c
Eugene <github@certalarm.ru>
2026-01-28 16:18:47 +0800

    TASK-2: Начал XmlDataWriter.

---
+++ RfrmLib/Data/Implementation/XmlDataWriter/XmlDataWriter.cs
@@ -0,0 +1,44 @@
+﻿using RfrmLib.Data.Interface;
+using RfrmLib.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RfrmLib.Data.Implementation.XmlDataWriter
+{
+    internal class XmlDataWriter : IXmlDataWriter
+    {
+        const string __employees = "Employees";
+        const string __employee = "Employee";
+
+        public string Write(IEnumerable<Employee> employees, string xmlFullFilename)
+        {
+            XmlWriterSettings xws = new XmlWriterSettings() { Indent = true };
+            using XmlWriter writer = XmlWriter.Create(xmlFullFilename, xws);
+            writer.WriteStartElement(__employees);
+
+            foreach (var employee in employees)
+            {
+                writer.WriteStartElement(__employee);
+                writer.WriteAttributeString(nameof(employee.Name).ToLower(), employee.Name);
+                writer.WriteAttributeString(nameof(employee.Surname).ToLower(), employee.Surname);
+                writer.WriteAttributeString(nameof(employee.SalarySum).ToLower(), AsString(employee.SalarySum));
+
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+        }
+
+        public string Write(Pay pay, string xmlFullFilename)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string AsString(double value) =>
+            value.ToString().Replace(",", ".");
+    }
+}
