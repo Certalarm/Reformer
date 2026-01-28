e4d78c4e00bb834503b7d84b2a6eca8f9013b076
Eugene <github@certalarm.ru>
2026-01-28 11:38:44 +0800

    TASK-2: Добавил модели дата-слоя.

---
+++ RfrmLib/Data/Models/EmployeeInput.cs
@@ -0,0 +1,16 @@
+﻿namespace RfrmLib.Data.Models
+{
+    internal class EmployeeInput
+    {
+        public string Name { get; }
+        public string Surname { get; }
+
+        #region .ctors
+        public EmployeeInput(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+        #endregion
+    }
+}
---
+++ RfrmLib/Data/Models/SalaryInput.cs
@@ -0,0 +1,16 @@
+﻿namespace RfrmLib.Data.Models
+{
+    internal class SalaryInput
+    {
+        public string Amount { get; }
+        public string Mount { get; }
+
+        #region .ctors
+        public SalaryInput(string amount, string mount)
+        {
+            Amount = amount;
+            Mount = mount;
+        }
+        #endregion
+    }
+}
