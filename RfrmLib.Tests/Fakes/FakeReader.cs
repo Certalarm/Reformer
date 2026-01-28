0ff6cafbf8fae021d22cfcda8420278c6b2a5648
Eugene <github@certalarm.ru>
2026-01-28 21:06:31 +0800

    TASK-2: Установил пакеты для unit-тестов, создал FakeReader и FakeWriter.

---
+++ RfrmLib.Tests/Fakes/FakeReader.cs
@@ -0,0 +1,15 @@
+﻿using RfrmLib.Data.Interface;
+using RfrmLib.Domain.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace RfrmLib.Tests.Fakes
+{
+    public class FakeReader : IXmlDataReader
+    {
+        public (IEnumerable<Employee>, string) Read(string xmlInputFileFullname, string xmlStyleSheetFullname)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RfrmLib.Tests/Fakes/FakeWriter.cs
@@ -0,0 +1,20 @@
+﻿using RfrmLib.Data.Interface;
+using RfrmLib.Domain.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace RfrmLib.Tests.Fakes
+{
+    public class FakeWriter : IXmlDataWriter
+    {
+        public string Write(IEnumerable<Employee> employees, string xmlFullFilename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Write(Pay pay, string xmlFullFilename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
