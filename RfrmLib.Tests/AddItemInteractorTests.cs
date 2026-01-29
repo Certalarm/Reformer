f88ffd89b0334ae28826bcd60e6b2a9a417f9105
Eugene <github@certalarm.ru>
2026-01-29 15:03:11 +0800

    TASK-2: Написал  и протестировал AddItemInteractor.

---
+++ RfrmLib.Tests/AddItemInteractorTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using RfrmLib.Domain.Entity;
+using RfrmLib.Domain.UseCase.AddItem;
+using RfrmLib.Tests.Fakes;
+using Xunit;
+using static RfrmLib.Utility.Txt;
+
+namespace RfrmLib.Tests
+{
+    public class AddItemInteractorTests
+    {
+        private readonly AddItemInteractor _sut;
+
+        #region .ctors
+        public AddItemInteractorTests()
+        {
+            var reader = new FakeReader();
+            _sut = new AddItemInteractor(reader);
+        }
+        #endregion
+
+        [Fact]
+        public void afterAdd_null_item_has_error()
+        {
+            (Pay pay, string error) = _sut.Execute("", null);
+
+            error.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public void afterAdd_item_totalAmount_will_changed()
+        {
+            var item = new Item("Lena", "Ivanova", "1234.3", "april");
+            var result = "13237" + __systemDecimalSeparator + "5";
+
+            (Pay pay, string error) = _sut.Execute("", item);
+            
+            pay.TotalAmount.Should().Be(result);
+        }
+    }
+}
---
+++ RfrmLib/Domain/UseCase/AddItem/AddItemInteractor.cs
@@ -0,0 +1,46 @@
+﻿using RfrmLib.Data.Interface;
+using RfrmLib.Domain.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using static RfrmLib.Domain.UseCase.InteractorHelper;
+using static RfrmLib.Utility.Txt;
+
+namespace RfrmLib.Domain.UseCase.AddItem
+{
+    public class AddItemInteractor
+    {
+        private readonly IXmlDataReader _reader;
+
+        #region .ctors
+        public AddItemInteractor(IXmlDataReader reader)
+        {
+            _reader = reader;    
+        }
+        #endregion
+
+        public (Pay, string) Execute(string inputDataFilename, Item item)
+        {
+            (Pay pay, string error) = _reader.Read(inputDataFilename);
+            if (error.Length > 0)
+                return (null, error);
+            error = AddItem(pay, item);
+            if (error.Length > 0)
+                return (null, error);
+            pay.TotalAmount = CalcTotalAmount(pay.Items);
+            return (pay, string.Empty);
+        }
+
+        private string AddItem(Pay pay, Item item)
+        {
+            if (item is null)
+                return __errorNullData;
+            pay.Items.Add(item);
+            return string.Empty;
+        }
+
+        private string CalcTotalAmount(IEnumerable<Item> items) =>
+            items
+                .Sum(x => ToDouble(x.Amount))
+                .ToString();
+    }
+}
