2bda73760e251f7e563caefc7ffddcd84990d98b
Eugene <github@certalarm.ru>
2026-01-30 16:09:30 +0800

    TASK-2: Написал обработчики событий для кнопок ""Запустить ...".

--- RfrmUI/MainWindow.xaml.cs
+++ RfrmUI/MainWindow.xaml.cs
@@ -3,25 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+using System.Windows.Threading;
 
 namespace RfrmUI
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const string __errorItem = "All Item fields must be filled in!";
         private const string __inputDir = "_inputdata";
         private const string __outputDir = "_outputdata";
         private const string __inputFilename = "data1.xml";
@@ -42,58 +31,56 @@ namespace RfrmUI
 
             Folders.Add(inputFullDir);
             Folders.Add(outputFullDir);
+            ReportLW.ItemsSource = Report;
+        }
 
+        private void Button_MainFlow_Clock(object sender, RoutedEventArgs e)
+        {
             var inputFullFilename = Path.Combine(Folders[0], __inputFilename);
-            var tramsformFullFilename = Path.Combine(Folders[0], __transformFilename);
+            var transformFullFilename = Path.Combine(Folders[0], __transformFilename);
             var outputFullFilename = Path.Combine(Folders[1], __outputFilename);
-
-
-            //AddItemController aiController = new AddItemController();
-            //var result = aiController.Handle(inputFullFilename, tramsformFullFilename, outputFullFilename,
-            //    "Lena|Ivanova|1234.5|april");
-
+            ProcessingMainFlow(inputFullFilename, transformFullFilename, outputFullFilename);
         }
 
-        private void ButtonIn_Click(object sender, RoutedEventArgs e)
+        private void Button_AddItem_Click(object sender, RoutedEventArgs e)
         {
-            //var dialog = new FolderBrowserDialog
-            //{
-            //    SelectedPath = Folders[0]
-            //};
-            //if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
-            //if (Folders[0] != dialog.SelectedPath)
-            //{
-            //    ClearLists();
-            //    Folders[0] = dialog.SelectedPath;
-            //    UpdateWatcher(Folders[0]);
-            //}
+            if (IsItemTBsNotEmpty())
+            {
+                var inputFullFilename = Path.Combine(Folders[0], __inputFilename);
+                var transformFullFilename = Path.Combine(Folders[0], __transformFilename);
+                var outputFullFilename = Path.Combine(Folders[1], __outputFilename);
+                var item = $"{NameTB.Text}|{SurnameTB.Text}|{AmountTB.Text}|{MountTB.Text}";
+                ProcessingAddItemFlow(inputFullFilename, transformFullFilename, outputFullFilename, item);
+            }
+            else
+            {
+                UpdateInvoke([__errorItem]);
+            }
         }
 
-        private void ButtonOut_Click(object sender, RoutedEventArgs e)
+        private void UpdateInvoke(IEnumerable<string> report)
         {
-            //var dialog = new FolderBrowserDialog
-            //{
-            //    SelectedPath = Folders[1]
-            //};
-            //if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
-            //if (Folders[1] != dialog.SelectedPath)
-            //{
-            //    Folders[1] = dialog.SelectedPath;
-            //}
+            Dispatcher.Invoke(() =>
+            {
+                Report.Clear();
+                foreach (var entry in report)
+                    Report.Add(entry);
+            });
         }
 
         private void ProcessingMainFlow(string inputFullFilename, string tramsformFullFilename, string outputFullFilename)
         {
             MainFlowController controller = new MainFlowController();
-            Report = new ObservableCollection<string>(
-                controller.Handle(inputFullFilename, tramsformFullFilename, outputFullFilename));
+            UpdateInvoke(controller.Handle(inputFullFilename, tramsformFullFilename, outputFullFilename));
         }
 
         private void ProcessingAddItemFlow(string inputFullFilename, string tramsformFullFilename, string outputFullFilename, string item)
         {
             AddItemController controller = new AddItemController();
-            Report = new ObservableCollection<string>(
-                controller.Handle(inputFullFilename, tramsformFullFilename, outputFullFilename, item));
+            UpdateInvoke(controller.Handle(inputFullFilename, tramsformFullFilename, outputFullFilename, item));
         }
+
+        private bool IsItemTBsNotEmpty() =>
+            NameTB.Text.Length > 0 && SurnameTB.Text.Length > 0 && AmountTB.Text.Length > 0 && MountTB.Text.Length > 0;
     }
 }
